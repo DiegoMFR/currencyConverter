aa12d7c73700d40d3a6910f099b9cb81cbee74f4
Diego Morales <dmorales@suitsupply.com>
2021-03-07 18:18:04 +0100

    update bundle name

--- stencil.config.ts
+++ stencil.config.ts
@@ -3,6 +3,7 @@ import { Config } from '@stencil/core';
 // https://stenciljs.com/docs/config
 
 export const config: Config = {
+  namespace: 'CurrencyConverter',
   globalStyle: 'src/global/global.css',
   globalScript: 'src/global/global.ts',
   taskQueue: 'async',
